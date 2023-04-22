5d1b3c1ba57f3226d475ff0dbc90efb2e9d3d649
Olivier Huin <olih@users.noreply.github.com>
2023-04-22 06:43:16 +0100

    model of error

--- src/api-model.ts
+++ src/api-model.ts
@@ -2,7 +2,48 @@ import { z } from 'zod';
 import { EngravingModel } from './engraving-model.js';
 import { Result } from './railway.js';
 
-type EngravingFunctionResult = Result<string, string>;
+interface EngravingInput {
+  name: string;
+  opts: object;
+  headers: object;
+  parameters: object;
+  payload: object;
+  context: object;
+}
+
+type schemaId = 'opts' | 'headers' | 'parameters' | 'payload' | 'context';
+
+type EngravingError =
+  | {
+      id: string;
+      engraving: string;
+      phase: 'validation';
+      schemas: schemaId[];
+      metadata: { [key: string]: string };
+      messages: string[];
+    }
+  | {
+      id: string;
+      engraving: string;
+      phase: 'actions';
+      action: string;
+      metadata: { [key: string]: string };
+      messages: string[];
+    }
+  | {
+      id: string;
+      engraving: string;
+      phase: 'onFinish';
+      metadata: { [key: string]: string };
+      messages: string[];
+    }
+  | {
+      phase: '_internal';
+      metadata: { [key: string]: string };
+      messages: string[];
+    };
+
+type EngravingFunctionResult = Result<EngravingInput, EngravingError>;
 
 type SyncEngravingFunction = () => EngravingFunctionResult;
 type AsyncEngravingFunction = () => Promise<EngravingFunctionResult>;
@@ -10,5 +51,5 @@ type AsyncEngravingFunction = () => Promise<EngravingFunctionResult>;
 export interface LunarDiamondEngavingOpts {
   model: EngravingModel;
   functions: { [name: string]: SyncEngravingFunction | AsyncEngravingFunction };
-  schemas: { [name: string]: z.AnyZodObject};
+  schemas: { [name: string]: z.AnyZodObject };
 }
