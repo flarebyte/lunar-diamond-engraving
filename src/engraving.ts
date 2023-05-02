b93b6a0c0b35a5c23b0ba0866ccc7e8232c7edad
Olivier Huin <olih@users.noreply.github.com>
2023-05-02 08:40:01 +0100

    Adds skeleton for validation and shield

--- src/engraving.ts
+++ src/engraving.ts
@@ -15,13 +15,142 @@ const runValidation = async (
   };
 
   if (validate.opts === undefined) {
-    throw Error(`${validation.check.opts} is not a validation function`);
+    throw Error(
+      `${validation.check.opts} used by ${mask.name} is not a validation function (ops)`
+    );
   }
-  const optsResult = await validate.opts({
-    target: 'opts',
-    object: mask.opts,
-    engravingInput: mask,
-  });
+  if (validate.headers === undefined) {
+    throw Error(
+      `${validation.check.headers} used by ${mask.name} is not a validation function (headers)`
+    );
+  }
+  if (validate.parameters === undefined) {
+    throw Error(
+      `${validation.check.parameters} used by ${mask.name} is not a validation function (parameters)`
+    );
+  }
+  if (validate.payload === undefined) {
+    throw Error(
+      `${validation.check.payload} used by ${mask.name} is not a validation function (payload)`
+    );
+  }
+  if (validate.context === undefined) {
+    throw Error(
+      `${validation.check.context} used by ${mask.name} is not a validation function (context)`
+    );
+  }
+
+  const results = {
+    opts: await validate.opts({
+      target: 'opts',
+      object: mask.opts,
+      engravingInput: mask,
+    }),
+    headers: await validate.opts({
+      target: 'headers',
+      object: mask.headers,
+      engravingInput: mask,
+    }),
+    parameters: await validate.opts({
+      target: 'parameters',
+      object: mask.parameters,
+      engravingInput: mask,
+    }),
+    payload: await validate.opts({
+      target: 'payload',
+      object: mask.payload,
+      engravingInput: mask,
+    }),
+    context: await validate.opts({
+      target: 'context',
+      object: mask.context,
+      engravingInput: mask,
+    }),
+  };
+
+  const isSuccess =
+    results.opts.status === 'success' &&
+    results.headers.status === 'success' &&
+    results.parameters.status === 'success' &&
+    results.payload.status === 'success' &&
+    results.context.status === 'success';
+  return { isSuccess, ...results };
+};
+
+const runShield = async (
+  shield: SingleEngravingModel['phases']['shield'],
+  mask: EngravingMask,
+  chisel: EngravingChisel
+) => {
+  const shieldfunc = {
+    opts: chisel.shieldFunctions[shield.check.opts],
+    headers: chisel.shieldFunctions[shield.check.headers],
+    parameters: chisel.shieldFunctions[shield.check.parameters],
+    payload: chisel.shieldFunctions[shield.check.payload],
+    context: chisel.shieldFunctions[shield.check.context],
+  };
+
+  if (shieldfunc.opts === undefined) {
+    throw Error(
+      `${shield.check.opts} used by ${mask.name} is not a shield function (ops)`
+    );
+  }
+  if (shieldfunc.headers === undefined) {
+    throw Error(
+      `${shield.check.headers} used by ${mask.name} is not a shield function (headers)`
+    );
+  }
+  if (shieldfunc.parameters === undefined) {
+    throw Error(
+      `${shield.check.parameters} used by ${mask.name} is not a shield function (parameters)`
+    );
+  }
+  if (shieldfunc.payload === undefined) {
+    throw Error(
+      `${shield.check.payload} used by ${mask.name} is not a shield function (payload)`
+    );
+  }
+  if (shieldfunc.context === undefined) {
+    throw Error(
+      `${shield.check.context} used by ${mask.name} is not a shield function (context)`
+    );
+  }
+
+  const results = {
+    opts: await shieldfunc.opts({
+      target: 'opts',
+      object: mask.opts,
+      engravingInput: mask,
+    }),
+    headers: await shieldfunc.opts({
+      target: 'headers',
+      object: mask.headers,
+      engravingInput: mask,
+    }),
+    parameters: await shieldfunc.opts({
+      target: 'parameters',
+      object: mask.parameters,
+      engravingInput: mask,
+    }),
+    payload: await shieldfunc.opts({
+      target: 'payload',
+      object: mask.payload,
+      engravingInput: mask,
+    }),
+    context: await shieldfunc.opts({
+      target: 'context',
+      object: mask.context,
+      engravingInput: mask,
+    }),
+  };
+
+  const isSuccess =
+    results.opts.status === 'success' &&
+    results.headers.status === 'success' &&
+    results.parameters.status === 'success' &&
+    results.payload.status === 'success' &&
+    results.context.status === 'success';
+  return { isSuccess, ...results };
 };
 
 export const runEngraving = async ({
@@ -36,5 +165,14 @@ export const runEngraving = async ({
     throw new Error(`Could not find engraving for ${mask.name}`);
   }
   const { validation, shield, actions, onFinish } = engraving.phases;
-  await runValidation(validation, mask, chisel);
+  const shieldResult = await runShield(shield, mask, chisel);
+  if (!shieldResult.isSuccess) {
+    // log issue
+    return;
+  }
+  const validationResult = await runValidation(validation, mask, chisel);
+  if (!validationResult.isSuccess) {
+    // log issue
+    return;
+  }
 };
