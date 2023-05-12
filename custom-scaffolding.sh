7397e03ebd40fd3d63490380bf12005b8a47ef92
Olivier Huin <olih@users.noreply.github.com>
2023-05-12 06:39:57 +0100

    broth doc ts uses mts and add function cs

--- custom-scaffolding.sh
+++ custom-scaffolding.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 npx zx --install https://raw.githubusercontent.com/flarebyte/baldrick-reserve/main/reserve-schema/schema-to-md.mjs --schema spec/snapshots/engraving-model/get-schema--schema.json --md SCHEMA.md --title "lunar-diamond-engraving"
-npx zx https://raw.githubusercontent.com/flarebyte/baldrick-reserve/main/script/npm-dependencies.mjs
-yarn cli parse --feature internal ngram functions-csv
 
 npx baldrick-broth@latest md fix
